153d55ff06d5baf0e08466e6035e32249bb505e4
ferradino <nicholasferradino@icloud.com>
2025-01-23 15:57:08 -0500

    Removed unnecessary code

--- src/main.c
+++ src/main.c
@@ -5,17 +5,11 @@
 #include "robot.h"
 
 int main(void) {
-  printf("*** Connecting to EV3 robot! ****\n");
-  startRobot();
-
   printf("*** Starting Motor! ****\n");
   rotateCube();
   
   printf("*** Attempting to Flip Cube ! ****\n");
   flipCube();
-
-  printf("*** Stopping EV3 robot! ****\n");
-  stopRobot();
 
   return 0;
 }
