79e531a892b09834c0ad9148ca47831feacc2716
ferradino <nferradino@gmail.com>
2025-04-01 21:55:50 -0400

    add rest of structure for table generating

--- external/gen.c
+++ external/gen.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "gen.h"
 
-void write_table_to_file(const unsigned char lookup[PART1_NUM_PERMUTATIONS]) {
+void write_table_to_file(const unsigned char *lookup, const char *file, const int n) {
     FILE *fp = fopen(PART1_TABLE_FILE, "w"); 
     assert(fp != NULL);
     for (int i = 0; i < PART1_NUM_PERMUTATIONS; i++) {
@@ -11,16 +11,16 @@ void write_table_to_file(const unsigned char lookup[PART1_NUM_PERMUTATIONS]) {
     fclose(fp);
 }
 
-unsigned short get_index(const unsigned char perm[NUM_EDGES]) {
+unsigned short get_index(const unsigned char *array, const int n) {
     unsigned short idx = 0;
-    for (int i = 0; i < NUM_EDGES; i++) {
-        idx |= (perm[i] << i);
+    for (int i = 0; i < n; i++) {
+        idx |= (array[i] << i);
     }
     return idx;
 }
 
 void generate_stage_one_table(rubix_cube_t cube) {
-    moves_t moves[PART1_MOVES] = { L, R, F, B, U, D }; 
+    moves_t moves[NUM_MOVES] = { L, R, F, B, U, D }; 
     unsigned char lookup[PART1_NUM_PERMUTATIONS];
     unsigned char permutations[PART1_NUM_PERMUTATIONS];
 
@@ -40,12 +40,50 @@ void generate_stage_one_table(rubix_cube_t cube) {
     while (!isEmpty(&queue)) {
         tmp = dequeue(&queue);
 
-        for (int i = 0; i < PART1_MOVES; i++) {
+        for (int i = 0; i < NUM_MOVES; i++) {
             tmp2 = tmp;
 
             make_move(&tmp2, moves[i]);
 
-            idx = get_index(tmp2.edge_orientation);
+            idx = get_index(tmp2.edge_orientation, PART1_NUM_PERMUTATIONS);
+            if (permutations[idx] == UNVISITED) {
+                permutations[idx] = 1;
+                lookup[idx] = (moves[i] + (unsigned char) 2);
+                enqueue(&queue, tmp2);
+            }
+        }
+    }
+
+    write_table_to_file(lookup, PART1_TABLE_FILE, PART1_NUM_PERMUTATIONS);
+}
+
+void generate_stage_two_table(rubix_cube_t cube) {
+    moves_t moves[NUM_MOVES] = { L, R, F, B, U2, D2 }; 
+    unsigned char lookup[PART2_NUM_PERMUTATIONS];
+    unsigned char permutations[PART2_NUM_PERMUTATIONS];
+
+    for (int i = 0; i < PART2_NUM_PERMUTATIONS; i++) {
+      permutations[i] = UNVISITED;
+    }
+
+    queue_t queue;
+    initQueue(&queue);
+
+    permutations[0] = 1; 
+    enqueue(&queue, cube);
+
+    unsigned short idx;
+    rubix_cube_t tmp, tmp2;
+
+    while (!isEmpty(&queue)) {
+        tmp = dequeue(&queue);
+
+        for (int i = 0; i < NUM_MOVES; i++) {
+            tmp2 = tmp;
+
+            make_move(&tmp2, moves[i]);
+
+            idx = get_index(tmp2.edge_orientation, PART2_NUM_PERMUTATIONS);
 
             if (permutations[idx] == UNVISITED) {
                 permutations[idx] = 1;
@@ -55,7 +93,85 @@ void generate_stage_one_table(rubix_cube_t cube) {
         }
     }
 
-    write_table_to_file(lookup);
+    write_table_to_file(lookup, PART2_TABLE_FILE, PART2_NUM_PERMUTATIONS);
+}
+
+void generate_stage_three_table(rubix_cube_t cube) {
+    moves_t moves[NUM_MOVES] = { L, R, F2, B2, U2, D2 }; 
+    unsigned char lookup[PART3_NUM_PERMUTATIONS];
+    unsigned char permutations[PART3_NUM_PERMUTATIONS];
+
+    for (int i = 0; i < PART3_NUM_PERMUTATIONS; i++) {
+      permutations[i] = UNVISITED;
+    }
+
+    queue_t queue;
+    initQueue(&queue);
+
+    permutations[0] = 1; 
+    enqueue(&queue, cube);
+
+    unsigned short idx;
+    rubix_cube_t tmp, tmp2;
+
+    while (!isEmpty(&queue)) {
+        tmp = dequeue(&queue);
+
+        for (int i = 0; i < NUM_MOVES; i++) {
+            tmp2 = tmp;
+
+            make_move(&tmp2, moves[i]);
+
+            idx = get_index(tmp2.edge_orientation, PART3_NUM_PERMUTATIONS);
+
+            if (permutations[idx] == UNVISITED) {
+                permutations[idx] = 1;
+                lookup[idx] = (moves[i] + (unsigned char) 2);
+                enqueue(&queue, tmp2);
+            }
+        }
+    }
+
+    write_table_to_file(lookup, PART3_TABLE_FILE, PART3_NUM_PERMUTATIONS);
+}
+
+void generate_stage_four_table(rubix_cube_t cube) {
+    moves_t moves[NUM_MOVES] = { L2, R2, F2, B2, U2, D2 }; 
+    unsigned char lookup[PART4_NUM_PERMUTATIONS];
+    unsigned char permutations[PART4_NUM_PERMUTATIONS];
+
+    for (int i = 0; i < PART1_NUM_PERMUTATIONS; i++) {
+      permutations[i] = UNVISITED;
+    }
+
+    queue_t queue;
+    initQueue(&queue);
+
+    permutations[0] = 1; 
+    enqueue(&queue, cube);
+
+    unsigned short idx;
+    rubix_cube_t tmp, tmp2;
+
+    while (!isEmpty(&queue)) {
+        tmp = dequeue(&queue);
+
+        for (int i = 0; i < NUM_MOVES; i++) {
+            tmp2 = tmp;
+
+            make_move(&tmp2, moves[i]);
+
+            idx = get_index(tmp2.edge_orientation, PART4_NUM_PERMUTATIONS);
+
+            if (permutations[idx] == UNVISITED) {
+                permutations[idx] = 1;
+                lookup[idx] = (moves[i] + (unsigned char) 2);
+                enqueue(&queue, tmp2);
+            }
+        }
+    }
+
+    write_table_to_file(lookup, PART4_TABLE_FILE, PART4_NUM_PERMUTATIONS);
 }
 
 void gen_tables(void) {
@@ -74,7 +190,8 @@ void gen_tables(void) {
         }
     }
 
-    printf("calling gen 1 tables");
-
     generate_stage_one_table(cube);
+    generate_stage_two_table(cube);
+    generate_stage_three_table(cube);
+    generate_stage_four_table(cube);
 }
--- external/gen.h
+++ external/gen.h
@@ -4,16 +4,25 @@
 #include "queue.h"
 #include "../include/cube.h"
 
+#define NUM_MOVES 6
 #define PART1_NUM_PERMUTATIONS 2048
-#define PART1_MOVES 6
 #define PART1_TABLE_FILE "./external/tables/stage1.txt"
+#define PART2_NUM_PERMUTATIONS 1082565
+#define PART2_TABLE_FILE "./external/tables/stage2.txt"
+#define PART3_NUM_PERMUTATIONS 29400 
+#define PART3_TABLE_FILE "./external/tables/stage3.txt"
+#define PART4_NUM_PERMUTATIONS 663552
+#define PART4_TABLE_FILE "./external/tables/stage4.txt"
 
 enum STATUS { UNVISITED, VISITED };
 
 void gen_tables(void);
 void generate_stage_one_table(rubix_cube_t cube);
+void generate_stage_two_table(rubix_cube_t cube);
+void generate_stage_three_table(rubix_cube_t cube);
+void generate_stage_four_table(rubix_cube_t cube);
 
-unsigned short get_index(const unsigned char perm[NUM_EDGES]);
-void write_table_to_file(const unsigned char table[PART1_NUM_PERMUTATIONS]);
+unsigned short get_index(const unsigned char *array, const int n);
+void write_table_to_file(const unsigned char *lookup, const char file[28], const int n);
 
 #endif
